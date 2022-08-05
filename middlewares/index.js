536f3419d5bd227d4fd93e20c7ad602d9e82eda5
zeeshan-thedeveloper <zeeshanahmedd0010@gmail.com>
2022-08-05 12:52:12 +0500

    create the first laayer of dev authenticaiton by implementation of jwt verfication

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,15 +1,18 @@
 const { verifyToken } = require("../token-manager/token-manager")
 
 const verifyJwt=(req,res,next) => {
+
     const authToken = req.headers.authorization.split(" ")[1];
+    console.log(req.body)
     if(authToken!=undefined) {
         let response = verifyToken(authToken);
-        if(response==true){
+        console.log("response",response)
+        if(response!="TokenExpiredError: jwt expired"){
             console.log("Token is verified");
             next();
         }else{
             res.status(501).send({
-                responseMessage:e
+                responseMessage:response
             })
         }
     }else{
