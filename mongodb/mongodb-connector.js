b7b2b705cd5770b169ae3cf0391333c898dd7793
Zeeshan <zeeshanahmedd0010@gmail.com>
2022-08-17 00:13:58 -0700

    final-v-1

--- mongodb/mongodb-connector.js
+++ mongodb/mongodb-connector.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.connect(
-  // `mongodb+srv://zeeshan:zeeshan@local-database-hosting.ipmhboj.mongodb.net/ldh-central-api?retryWrites=true&w=majority&ssl=true`, 
-  `mongodb://zeeshan:m9mg1AF0REGsqbsa@ac-z7spiov-shard-00-00.ipmhboj.mongodb.net:27017,ac-z7spiov-shard-00-01.ipmhboj.mongodb.net:27017,ac-z7spiov-shard-00-02.ipmhboj.mongodb.net:27017/ldh-central-api?ssl=true&replicaSet=atlas-68jhda-shard-0&authSource=admin&retryWrites=true&w=majority`,
+  `mongodb+srv://zeeshan:zeeshan@local-database-hosting.ipmhboj.mongodb.net/ldh-central-api?retryWrites=true&w=majority&ssl=true`, 
+  // `mongodb://zeeshan:m9mg1AF0REGsqbsa@ac-z7spiov-shard-00-00.ipmhboj.mongodb.net:27017,ac-z7spiov-shard-00-01.ipmhboj.mongodb.net:27017,ac-z7spiov-shard-00-02.ipmhboj.mongodb.net:27017/ldh-central-api?ssl=true&replicaSet=atlas-68jhda-shard-0&authSource=admin&retryWrites=true&w=majority`,
   // `mongodb://127.0.0.1:27017/ldh-central-api`,
   {
     useNewUrlParser: true, 
